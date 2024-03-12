d870932b53733bd8ce4683a7059e5242d2dd29ca
Acrilex23 <aroeiramartim90@gmail.com>
2024-03-12 14:09:14 +0000

    Added simple.py

---
+++ simple.py
@@ -0,0 +1,7 @@
+# Simple Python Script
+
+# Prompt the user for their name
+user_name = input("Enter your name: ")
+
+# Greet the user
+print(f"Hello, {user_name}! Welcome to the world of Python scripting.")
